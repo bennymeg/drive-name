046d0df4b096c795eeb5800dc98efb11cbed1e36
Benny Megidish <bennymegk@gmail.com>
2020-02-24 01:38:09 +0200

    added more tests

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 const driveName = require('../drive_name');
+const driveList = require('drivelist');
 
 async function getDriveNameTest(driveMountPoint) {
   console.log();
@@ -7,7 +8,22 @@ async function getDriveNameTest(driveMountPoint) {
   console.log(`Drive: '${driveMountPoint}' Name: '${driveName.getDriveName(driveMountPoint)}'`);
 }
 
+async function getAllDriveNameTest() {
+  console.log();
+  console.log('getAllDriveName() Test:');
+  console.log();
+
+  // print all the drives and their names respectively
+  driveList.list().then(drives => {
+    drives.forEach(drive => {
+        drive.mountpoints.forEach(mountpoint => {
+          console.log(`Drive: '${mountpoint.path}' Name: '${driveName.getDriveName(mountpoint.path)}'`);
+        });
+    });
+  });
+}
+
 // run tests
 getDriveNameTest('D://');
 getDriveNameTest('E://');
-
+getAllDriveNameTest();
